13cf67bb61c98f9c569cd236e4d7c6372a6ac05f
Helena Meléndez <helenamel98@gmail.com>
2025-07-24 14:34:18 -0400

    Update data.js

--- js/data.js
+++ js/data.js
@@ -10,4 +10,28 @@ const cursos = [
         semestre: "Año 1 - Semestre 1",
         prerrequisitos: []
     }
+{
+    name: "BIOLOGÍA CELULAR Y GENÉTICA",
+    semester: 1,
+    year: 1,
+    prereqs: [],
+  },
+  {
+    name: "SALUD COMUNITARIA 1",
+    semester: 2,
+    year: 1,
+    prereqs: [],
+  },
+  {
+    name: "BIOQUÍMICA",
+    semester: 2,
+    year: 1,
+    prereqs: ["QUÍMICA GENERAL Y ORGÁNICA", "BIOLOGÍA CELULAR Y GENÉTICA"],
+  },
+  {
+    name: "FUNDAMENTOS DE ENFERMERÍA 2",
+    semester: 1,
+    year: 2,
+    prereqs: ["HISTOLOGÍA", "ANATOMÍA", "INTEGRACIÓN AL DESEMPEÑO PROFESIONAL 1", "FISIOLOGÍA GENERAL"],
+  }
 ];
