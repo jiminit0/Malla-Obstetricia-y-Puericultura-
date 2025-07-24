288bcd95ab3aff2c5ab107b8cbc2112dc1d52740
Helena Meléndez <helenamel98@gmail.com>
2025-07-24 15:41:30 -0400

    data.js

--- js/data.js
+++ js/data.js
@@ -11,6 +11,86 @@ const curso = [
         prerrequisitos: []
     }
     {
+        nombre: "BIOLOGÍA CELULAR Y GENÉTICA",
+        semestre: "Año 1 - Semestre 1",
+        prerrequisitos: []
+    },
+    {
+        nombre: "MATEMÁTICAS",
+        semestre: "Año 1 - Semestre 1",
+        prerrequisitos: []
+    },
+    {
+        nombre: "FÍSICA",
+        semestre: "Año 1 - Semestre 1",
+        prerrequisitos: []
+    },
+    {
+        nombre: "EDUCACIÓN EN SALUD 1",
+        semestre: "Año 1 - Semestre 1",
+        prerrequisitos: []
+    },
+    {
+        nombre: "CIENCIAS SOCIALES Y SALUD 1",
+        semestre: "Año 1 - Semestre 1",
+        prerrequisitos: []
+    },
+    {
+        nombre: "CURSO DE FORMACIÓN GENERAL 1",
+        semestre: "Año 1 - Semestre 1",
+        prerrequisitos: []
+    },
+    {
+        nombre: "INGLÉS 1",
+        semestre: "Año 1 - Semestre 1",
+        prerrequisitos: []
+    },
+    {
+        nombre: "SALUD COMUNITARIA 1",
+        semestre: "Año 1 - Semestre 2",
+        prerrequisitos: []
+    },
+    {
+        nombre: "BIOQUÍMICA",
+        semestre: "Año 1 - Semestre 2",
+        prerrequisitos: ["QUÍMICA GENERAL Y ORGÁNICA", "BIOLOGÍA CELULAR Y GENÉTICA"]
+    },
+    {
+        nombre: "BIOLOGÍA DEL DESARROLLO Y EMBRIOLOGÍA HUMANA",
+        semestre: "Año 1 - Semestre 2",
+        prerrequisitos: ["BIOLOGÍA CELULAR Y GENÉTICA"]
+    },
+    {
+        nombre: "FISIOLOGÍA GENERAL",
+        semestre: "Año 1 - Semestre 2",
+        prerrequisitos: ["FÍSICA", "BIOLOGÍA CELULAR Y GENÉTICA"]
+    },
+    {
+        nombre: "ANATOMÍA",
+        semestre: "Año 1 - Semestre 2",
+        prerrequisitos: ["BIOLOGÍA CELULAR Y GENÉTICA"]
+    },
+    {
+        nombre: "HISTOLOGÍA",
+        semestre: "Año 1 - Semestre 2",
+        prerrequisitos: ["BIOLOGÍA CELULAR Y GENÉTICA"]
+    },
+    {
+        nombre: "INTEGRACIÓN AL DESEMPEÑO PROFESIONAL 1",
+        semestre: "Año 1 - Semestre 2",
+        prerrequisitos: ["FUNDAMENTOS DE ENFERMERÍA 1"]
+    },
+    {
+        nombre: "FUNDAMENTOS DE ENFERMERÍA 1",
+        semestre: "Año 1 - Semestre 1",
+        prerrequisitos: []
+    },
+    {
+        nombre: "FUNDAMENTOS DE ENFERMERÍA 1",
+        semestre: "Año 1 - Semestre 1",
+        prerrequisitos: []
+    },
+    {
         nombre: "FUNDAMENTOS DE ENFERMERÍA 1",
         semestre: "Año 1 - Semestre 1",
         prerrequisitos: []
@@ -30,4 +110,20 @@ const curso = [
         semestre: "Año 1 - Semestre 1",
         prerrequisitos: []
     },
+    {
+        nombre: "FUNDAMENTOS DE ENFERMERÍA 1",
+        semestre: "Año 1 - Semestre 1",
+        prerrequisitos: []
+    },
+    {
+        nombre: "FUNDAMENTOS DE ENFERMERÍA 1",
+        semestre: "Año 1 - Semestre 1",
+        prerrequisitos: []
+    },
+    {
+        nombre: "FUNDAMENTOS DE ENFERMERÍA 1",
+        semestre: "Año 1 - Semestre 1",
+        prerrequisitos: []
+    },
+
 ];
