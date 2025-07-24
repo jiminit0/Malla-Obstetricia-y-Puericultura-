f28f925f77d2e729d69b2d8bb9972057c39df4e1
Helena Meléndez <helenamel98@gmail.com>
2025-07-24 15:08:19 -0400

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -1,2 +1,13 @@
-// Cursos serán cargados aquí
-const cursos = [];
+
+const cursos = [
+    {
+        nombre: "FUNDAMENTOS DE ENFERMERÍA 1",
+        semestre: "Año 1 - Semestre 1",
+        prerrequisitos: []
+    },
+    {
+        nombre: "QUÍMICA GENERAL Y ORGÁNICA",
+        semestre: "Año 1 - Semestre 1",
+        prerrequisitos: []
+    }
+];
