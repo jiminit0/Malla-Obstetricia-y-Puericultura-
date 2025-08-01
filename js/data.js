7f4ac5f30a549a3d2bad06a5f4f1157b6c1d74a2
Helena Meléndez <helenamel98@gmail.com>
2025-08-01 15:08:11 -0400

    data.js

--- js/data.js
+++ js/data.js
@@ -3,142 +3,170 @@ const cursos = [
     {
         nombre: "FUNDAMENTOS DE ENFERMERÍA 1",
         semestre: "Año 1 - Semestre 1",
-        prerrequisitos: []
+        prerrequisitos: [],
+        creditos: 3
     },
     {
         nombre: "QUÍMICA GENERAL Y ORGÁNICA",
         semestre: "Año 1 - Semestre 1",
-        prerrequisitos: []
+        prerrequisitos: [],
+        creditos: 3
     },
     {
         nombre: "BIOLOGÍA CELULAR Y GENÉTICA",
         semestre: "Año 1 - Semestre 1",
-        prerrequisitos: []
+        prerrequisitos: [],
+        creditos: 6
     },
     {
         nombre: "MATEMÁTICAS",
         semestre: "Año 1 - Semestre 1",
-        prerrequisitos: []
+        prerrequisitos: [],
+        creditos: 2
     },
     {
         nombre: "FÍSICA",
         semestre: "Año 1 - Semestre 1",
-        prerrequisitos: []
+        prerrequisitos: [],
+        creditos: 2
     },
     {
         nombre: "EDUCACIÓN EN SALUD 1",
         semestre: "Año 1 - Semestre 1",
-        prerrequisitos: []
+        prerrequisitos: [],
+        creditos: 4
     },
     {
         nombre: "CIENCIAS SOCIALES Y SALUD 1",
         semestre: "Año 1 - Semestre 1",
-        prerrequisitos: []
+        prerrequisitos: [],
+        creditos: 6
     },
     {
         nombre: "CURSO DE FORMACIÓN GENERAL 1",
         semestre: "Año 1 - Semestre 1",
-        prerrequisitos: []
+        prerrequisitos: [],
+        creditos: 2
     },
     {
         nombre: "INGLÉS 1",
         semestre: "Año 1 - Semestre 1",
-        prerrequisitos: []
+        prerrequisitos: [],
+        creditos: 3
     },
     {
         nombre: "SALUD COMUNITARIA 1",
         semestre: "Año 1 - Semestre 2",
-        prerrequisitos: []
+        prerrequisitos: [],
+        creditos: 4
     },
     {
         nombre: "BIOQUÍMICA",
         semestre: "Año 1 - Semestre 2",
-        prerrequisitos: ["QUÍMICA GENERAL Y ORGÁNICA", "BIOLOGÍA CELULAR Y GENÉTICA"]
+        prerrequisitos: ["QUÍMICA GENERAL Y ORGÁNICA", "BIOLOGÍA CELULAR Y GENÉTICA"],
+        creditos: 3
     },
     {
         nombre: "BIOLOGÍA DEL DESARROLLO Y EMBRIOLOGÍA HUMANA",
         semestre: "Año 1 - Semestre 2",
-        prerrequisitos: ["BIOLOGÍA CELULAR Y GENÉTICA"]
+        prerrequisitos: ["BIOLOGÍA CELULAR Y GENÉTICA"],
+        creditos: 4
     },
     {
         nombre: "FISIOLOGÍA GENERAL",
         semestre: "Año 1 - Semestre 2",
-        prerrequisitos: ["FÍSICA", "BIOLOGÍA CELULAR Y GENÉTICA"]
+        prerrequisitos: ["FÍSICA", "BIOLOGÍA CELULAR Y GENÉTICA"],
+        creditos: 4
     },
     {
         nombre: "ANATOMÍA",
         semestre: "Año 1 - Semestre 2",
-        prerrequisitos: ["BIOLOGÍA CELULAR Y GENÉTICA"]
+        prerrequisitos: ["BIOLOGÍA CELULAR Y GENÉTICA"],
+        creditos: 5
     },
     {
         nombre: "HISTOLOGÍA",
         semestre: "Año 1 - Semestre 2",
-        prerrequisitos: ["BIOLOGÍA CELULAR Y GENÉTICA"]
+        prerrequisitos: ["BIOLOGÍA CELULAR Y GENÉTICA"],
+        creditos: 3
     },
     {
         nombre: "INTEGRACIÓN AL DESEMPEÑO PROFESIONAL 1",
         semestre: "Año 1 - Semestre 2",
-        prerrequisitos: ["FUNDAMENTOS DE ENFERMERÍA 1"]
+        prerrequisitos: ["FUNDAMENTOS DE ENFERMERÍA 1"],
+        creditos: 2
     },
     {
         nombre: "CURSO DE FORMACION GENERAL 2",
         semestre: "Año 1 - Semestre 2",
-        prerrequisitos: []
+        prerrequisitos: [],
+        creditos: 2
     },
     {
         nombre: "INGLÉS 2",
         semestre: "Año 1 - Semestre 2",
-        prerrequisitos: ["INGLÉS 1"]
+        prerrequisitos: ["INGLÉS 1"],
+        creditos: 3
     },
     {
         nombre: "FUNDAMENTOS DE ENFERMERÍA 2",
         semestre: "Año 2 - Semestre 1",
-        prerrequisitos: ["HISTOLOGÍA", "ANATOMÍA", "INTEGRACIÓN AL DESEMPEÑO PROFESIONAL 1", "FISIOLOGÍA GENERAL"]
+        prerrequisitos: ["HISTOLOGÍA", "ANATOMÍA", "INTEGRACIÓN AL DESEMPEÑO PROFESIONAL 1", "FISIOLOGÍA GENERAL"],
+        creditos: 4
     },
     {
         nombre: "OBSTETRICIA FISIOLÓGICA 1",
         semestre: "Año 2 - Semestre 1",
-        prerrequisitos: ["BIOLOGÍA DEL DESARROLLO Y EMBRIOLOGÍA HUMANA", "FISIOLOGÍA GENERAL", "HISTOLOGÍA", "ANATOMÍA", "INTEGRACIÓN AL DESEMPEÑO PROFESIONAL 1"]
+        prerrequisitos: ["BIOLOGÍA DEL DESARROLLO Y EMBRIOLOGÍA HUMANA", "FISIOLOGÍA GENERAL", "HISTOLOGÍA", "ANATOMÍA", "INTEGRACIÓN AL DESEMPEÑO PROFESIONAL 1"],
+        creditos: 4
     },
     {
         nombre: "NEONATOLOGÍA 1",
         semestre: "Año 2 - Semestre 1",
-        prerrequisitos: ["BIOLOGÍA DEL DESARROLLO Y EMBRIOLOGÍA HUMANA", "FISIOLOGÍA GENERAL", "HISTOLOGÍA", "ANATOMÍA", "INTEGRACIÓN AL DESEMPEÑO PROFESIONAL 1"]
+        prerrequisitos: ["BIOLOGÍA DEL DESARROLLO Y EMBRIOLOGÍA HUMANA", "FISIOLOGÍA GENERAL", "HISTOLOGÍA", "ANATOMÍA", "INTEGRACIÓN AL DESEMPEÑO PROFESIONAL 1"],
+        creditos: 4
     },
     {
         nombre: "FISIOLOGÍA DE SISTEMAS",
         semestre: "Año 2 - Semestre 1",
-        prerrequisitos: ["FISIOLOGÍA GENERAL"]
+        prerrequisitos: ["FISIOLOGÍA GENERAL"],
+        creditos: 5
     },
     {
         nombre: "INMUNOLOGÍA",
         semestre: "Año 2 - Semestre 1",
-        prerrequisitos: ["FISIOLOGÍA GENERAL"]
+        prerrequisitos: ["FISIOLOGÍA GENERAL"],
+        creditos: 3
     },
     {
         nombre: "AGENTES VIVOS DE ENFERMEDAD",
         semestre: "Año 2 - Semestre 1",
-        prerrequisitos: ["FISIOLOGÍA GENERAL", "HISTOLOGÍA"]
+        prerrequisitos: ["FISIOLOGÍA GENERAL", "HISTOLOGÍA"],
+        creditos: 3
     },
     {
         nombre: "CIENCIAS SOCIALES Y SALUD 2",
         semestre: "Año 2 - Semestre 1",
-        prerrequisitos: ["CIENCIAS SOCIALES Y SALUD 1"]
+        prerrequisitos: ["CIENCIAS SOCIALES Y SALUD 1"],
+        creditos: 5
     },
     {
         nombre: "INGLÉS 3",
         semestre: "Año 2 - Semestre 1",
-        prerrequisitos: ["INGLÉS 2"]
+        prerrequisitos: ["INGLÉS 2"],
+        creditos: 3
     },
     {
         nombre: "NEONATOLOGÍA 2",
         semestre: "Año 2 - Semestre 2",
-        prerrequisitos: ["AGENTES VIVOS DE ENFERMEDAD", "NEONATOLOGÍA 1", "FISIOLOGÍA DE SISTEMAS"]
+        prerrequisitos: ["AGENTES VIVOS DE ENFERMEDAD", "NEONATOLOGÍA 1", "FISIOLOGÍA DE SISTEMAS"],
+        creditos: 3
     },
     {
         nombre: "OBSTETRICIA FISIOLÓGICA 2",
         semestre: "Año 2 - Semestre 2",
-        prerrequisitos: ["OBSTETRICIA FISIOLÓGICA 1", "FISIOLOGÍA DE SISTEMAS"]
+        prerrequisitos: ["OBSTETRICIA FISIOLÓGICA 1", "FISIOLOGÍA DE SISTEMAS"],
+        creditos: 3
     },
 
 ];
